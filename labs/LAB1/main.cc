e4231d02e5f48e388794eba7dd6d74ea4ad3a7b5
Naumikhin Daniil <dan.naumihin@yandex.ru>
2023-09-25 11:01:42 +0300

    Отрисовка, GUI, Lab1
    Мне не нравится, как реализован Circle: на него ложится слишком много (хранение информации, отрисовка, GUI).
    Надо хотя бы переместить GUI в App --- так хоть как-то лучше будет.

--- labs/LAB1/main.cc
+++ labs/LAB1/main.cc
@@ -1,61 +1,247 @@
 #include <SDL2/SDL.h>
+#include <SDL_events.h>
 #include <SDL_render.h>
 #include <SDL_surface.h>
+
 #include <SDL_video.h>
+#include <imgui.h>
+#include <imgui_impl_sdl2.h>
+#include <imgui_impl_sdlrenderer2.h>
+
 #include <iostream>
+#include <iterator>
+#include <queue>
+#include <random>
+#include <sstream>
+#include <stdint.h>
+#include <string>
 
-void DrawCircle(SDL_Renderer *renderer, int32_t centreX, int32_t centreY,
-                int32_t radius) {
-  const int32_t diameter = (radius * 2);
+struct sdl_error : public std::runtime_error {
+  sdl_error(const char *s) : std::runtime_error(s) {}
+};
 
-  int32_t x = (radius - 1);
-  int32_t y = 0;
-  int32_t tx = 1;
-  int32_t ty = 1;
-  int32_t error = (tx - diameter);
+struct VR2 {
+  int32_t x, y;
+};
 
-  while (x >= y) {
-    //  Each of the following renders an octant of the circle
-    SDL_RenderDrawPoint(renderer, centreX + x, centreY - y);
-    SDL_RenderDrawPoint(renderer, centreX + x, centreY + y);
-    SDL_RenderDrawPoint(renderer, centreX - x, centreY - y);
-    SDL_RenderDrawPoint(renderer, centreX - x, centreY + y);
-    SDL_RenderDrawPoint(renderer, centreX + y, centreY - x);
-    SDL_RenderDrawPoint(renderer, centreX + y, centreY + x);
-    SDL_RenderDrawPoint(renderer, centreX - y, centreY - x);
-    SDL_RenderDrawPoint(renderer, centreX - y, centreY + x);
+VR2 operator+(const VR2 &a, const VR2 &b) { return {a.x + b.x, a.y + b.y}; }
 
-    if (error <= 0) {
-      ++y;
-      error += ty;
-      ty += 2;
-    }
+VR2 operator*(int32_t a, const VR2 &b) { return {a * b.x, a * b.y}; }
 
-    if (error > 0) {
-      --x;
-      tx += 2;
-      error += (tx - diameter);
+int32_t abs(VR2 v) { return v.x * v.x + v.y * v.y; }
+
+class Circle {
+  VR2 xy_;
+  int32_t r_;
+  bool show_gui = false, show_ = true;
+  std::default_random_engine gen;
+  std::uniform_int_distribution<> dist{-50, 50};
+
+public:
+  Circle(VR2 xy, uint32_t r) : xy_(xy), r_(r), gen(std::random_device{}()) {}
+
+  static int32_t po(const VR2 &x, const VR2 &y) {
+    auto dx = y.x - x.x, dy = y.y - x.y;
+    return dx * dx + dy * dy;
+    // return dx * dx + 4 * dy * dy;
+    // return (std::abs(dx) + std::abs(dy)) * (std::abs(dx) + std::abs(dy));
+    // return std::abs(dx*dy);
+    // return (std::abs(dx) + std::abs(4 * dy)) *
+    //        (std::abs(dx) + std::abs(4 * dy));
+  }
+
+  void Show(SDL_Renderer *s) {
+    if (!show_)
+      return;
+    auto sqrr = r_ * r_;
+    SDL_RenderDrawPoint(s, xy_.x, xy_.y); // <-- Center
+    VR2 cur = VR2{r_, 0};                 // delta
+    VR2 O = {0, 0};
+    while (cur.x > 0) {
+      auto sx = cur + VR2{-1, 0}, sy = cur + VR2{0, 1};
+      // std::cout << std::abs(po(sx, O) - sqrr) << " "
+      //           << std::abs(po(sy, O) - sqrr) << "\n";
+
+      SDL_RenderDrawPoint(s, xy_.x + cur.x, xy_.y + cur.y);
+      SDL_RenderDrawPoint(s, xy_.x - cur.x, xy_.y + cur.y);
+      SDL_RenderDrawPoint(s, xy_.x + cur.x, xy_.y - cur.y);
+      SDL_RenderDrawPoint(s, xy_.x - cur.x, xy_.y - cur.y);
+      //
+      // SDL_RenderDrawPoint(s, xy_.x + cur.y, xy_.y + cur.x);
+      // SDL_RenderDrawPoint(s, xy_.x - cur.y, xy_.y + cur.x);
+      // SDL_RenderDrawPoint(s, xy_.x + cur.y, xy_.y - cur.x);
+      // SDL_RenderDrawPoint(s, xy_.x - cur.y, xy_.y - cur.x);
+
+      if (std::abs(po(sx, O) - sqrr) < std::abs(po(sy, O) - sqrr))
+        cur = sx;
+      else
+        cur = sy;
     }
   }
-}
 
-int main(int argc, char *argv[]) {
-  SDL_Init(SDL_INIT_EVERYTHING);
-  SDL_Window *win =
-      SDL_CreateWindow("hello, circle", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
-  SDL_Renderer *ren = SDL_CreateRenderer(
-      win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+  void GUI_Show() {
+    if (!show_gui)
+      return;
+    ImGui::Begin("Circle", &show_gui);
+    ImGui::SliderInt("Radius", &r_, 0, 1000);
+    ImGui::SliderInt2("Coordinates", &xy_.x, -500, 500);
+    ImGui::Checkbox("Show", &show_);
+    if (ImGui::Button("Random move")) {
+      MoveTo({dist(gen), dist(gen)});
+    }
+    ImGui::End();
+  }
 
-  SDL_SetRenderDrawColor(ren, 0xFF, 0xFF, 0xFF, 0xFF);
-  SDL_RenderClear(ren);
+  bool &GetGuiTrigger() { return show_gui; }
 
-  SDL_SetRenderDrawColor(ren, 0x00, 0x00, 0x00, 0xFF);
-  DrawCircle(ren, 150, 150, 50);
-  SDL_RenderPresent(ren);
-  SDL_Delay(2000);
+  uint32_t GetRadius() const { return r_; }
 
-  SDL_DestroyRenderer(ren);
-  SDL_DestroyWindow(win);
+  VR2 GetPostion() const { return xy_; }
+
+  void SetRadius(uint32_t r) { r_ = r; }
+
+  void SetPosition(const VR2 &pos) { xy_ = pos; }
+
+  void MoveTo(const VR2 &dlta) { xy_ = xy_ + dlta; }
+};
+
+class App {
+  SDL_Window *win_ =
+      SDL_CreateWindow("Lab 1", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+  SDL_Renderer *ren_ = SDL_CreateRenderer(
+      win_, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+  bool running_ = true;
+  float color_[3] = {0.f, 0.f, 0.f};
+  char windowtitle_[255] = "Lab 1";
+  bool show_settings_ = false;
+  Circle *c_ = new Circle({150, 150}, 50);
+
+public:
+  App() {
+    if (win_ == NULL)
+      throw sdl_error("SDL_CreateWindow error");
+    if (ren_ == NULL)
+      throw sdl_error("SDL_CreateRenderer error");
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    ImGui_ImplSDL2_InitForSDLRenderer(win_, ren_);
+    ImGui_ImplSDLRenderer2_Init(ren_);
+  }
+  App(const App &) = delete;
+  App(App &&) = delete;
+  App &operator=(const App &) = delete;
+  App &operator=(App &&) = delete;
+
+  ~App() {
+    ImGui_ImplSDLRenderer2_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_DestroyRenderer(ren_);
+    SDL_DestroyWindow(win_);
+  }
+
+  void run() {
+    running_ = true;
+    while (running_) {
+      processEvent();
+      imGuiRender();
+      update();
+      render();
+    }
+  }
+
+  float get_scale() const {
+    int window_width = 0;
+    int windows_height = 0;
+    SDL_GetWindowSize(win_, &window_width, &windows_height);
+
+    int render_output_width = 0;
+    int render_output_height = 0;
+    SDL_GetRendererOutputSize(ren_, &render_output_width,
+                              &render_output_height);
+
+    const auto scale_x = static_cast<float>(render_output_width) /
+                         static_cast<float>(window_width);
+    return scale_x;
+  }
+
+private:
+  void processEvent() {
+    SDL_Event e{};
+    while (SDL_PollEvent(&e) != 0) {
+      ImGui_ImplSDL2_ProcessEvent(&e);
+      if (e.type == SDL_QUIT)
+        running_ = false;
+    }
+  }
+
+  void update() {}
+
+  void imGuiRender() {
+    ImGui_ImplSDLRenderer2_NewFrame();
+    ImGui_ImplSDL2_NewFrame();
+    ImGui::NewFrame();
+
+    if (ImGui::BeginMainMenuBar()) {
+      if (ImGui::BeginMenu("Window")) {
+        ImGui::MenuItem("Settings", nullptr, &show_settings_);
+        ImGui::EndMenu();
+      }
+      if (ImGui::BeginMenu("Figures")) {
+        ImGui::MenuItem("Circle", nullptr, &(c_->GetGuiTrigger()));
+        ImGui::EndMenu();
+      }
+      ImGui::EndMainMenuBar();
+    }
+    if (show_settings_) {
+      ImGui::Begin("Window settings", &show_settings_);
+      ImGui::ColorEdit3("Backgound color", color_);
+      ImGui::InputText("Window title", windowtitle_, 255);
+      if (ImGui::Button("Update window title"))
+        SDL_SetWindowTitle(win_, windowtitle_);
+      ImGui::End();
+    }
+    c_->GUI_Show();
+    ImGui::Render();
+  }
+
+  void render() {
+    SDL_SetRenderDrawColor(ren_, static_cast<uint8_t>(color_[0] * 255),
+                           static_cast<uint8_t>(color_[1] * 255),
+                           static_cast<uint8_t>(color_[2] * 255), 0xFF);
+    SDL_RenderClear(ren_);
+    SDL_SetRenderDrawColor(ren_, 0xFF, 0xFF, 0xFF, 0xFF);
+    c_->Show(ren_);
+
+    ImGui_ImplSDLRenderer2_RenderDrawData(ImGui::GetDrawData());
+
+    SDL_RenderPresent(ren_);
+  }
+};
+
+int main(int argc, char **argv) try {
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    throw sdl_error("SDL_Init");
+  {
+    App app;
+    app.run();
+  }
   SDL_Quit();
+
   return 0;
+} catch (sdl_error &e) {
+  std::cerr << "SDL error: " << e.what() << std::endl;
+  return 1;
+} catch (std::runtime_error &e) {
+  std::cerr << "Runtime error: " << e.what() << std::endl;
+  return 1;
+} catch (std::exception &e) {
+  std::cerr << "Standard exception: " << e.what() << std::endl;
+  return 1;
+} catch (...) {
+  std::cerr << "Unknown error" << std::endl;
+  return 1;
 }
--- labs/test/main.cc
+++ labs/test/main.cc
@@ -2,13 +2,14 @@
 #include <SDL_events.h>
 #include <SDL_surface.h>
 
+#include <imgui.h>
 #include <imgui_impl_sdl2.h>
 #include <imgui_impl_sdlrenderer2.h>
-#include <imgui.h>
 
 #include <iostream>
-#include <vector>
-
+#include <queue>
+#include <sstream>
+#include <stdint.h>
 
 struct sdl_error : public std::runtime_error {
   sdl_error(const char *s) : std::runtime_error(s) {}
@@ -26,6 +27,32 @@ struct Circle {
 struct ColoredCircle {
   Circle cr;
   uint8_t r, g, b, a;
+};
+
+class ImGuiPanel {
+  std::string name_;
+
+public:
+  ImGuiPanel(const std::string &name) : name_(name) {}
+  virtual void func() = 0;
+  virtual void show() {
+    ImGui::Begin(name_.c_str());
+    func();
+    ImGui::End();
+  }
+  virtual ~ImGuiPanel() = default;
+};
+
+struct ImGuiOCPanel : ImGuiPanel {
+  bool show_;
+  ImGuiOCPanel(std::string name, bool show) : ImGuiPanel(name), show_(show) {}
+  void func() override = 0;
+  void show() override {
+    if (show_) {
+      ImGuiPanel::show();
+    }
+  }
+  virtual ~ImGuiOCPanel() = default;
 };
 
 void Show(SDL_Renderer *s, const Circle &c) {
@@ -67,7 +94,23 @@ void Show(SDL_Renderer *s, const ColoredCircle &c) {
   Show(s, c.cr);
 }
 
+class ImGuiRenderer;
+
+void Show(const ImGuiRenderer &s, const Circle &c) {
+  std::stringstream ss;
+  ss << "Coordinates: (" << c.xy.x << ", " << c.xy.y << ")\n"
+     << "Radius: "
+     << "c.r";
+  std::string ans;
+  ss >> ans;
+  ImGui::Begin("Circle");
+  ImGui::Text("%s", ans.c_str());
+  ImGui::End();
+}
+
 #include "SFG/Showable.hh"
+using SDL_Showable = Showable<SDL_Renderer *>;
+using GUI_Showable = Showable<const ImGuiRenderer &>;
 
 class App {
   SDL_Window *win_ =
@@ -75,6 +118,10 @@ class App {
   SDL_Renderer *ren_ = SDL_CreateRenderer(
       win_, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
   bool running_ = true;
+  float color_[3] = {0.f, 0.f, 0.f};
+  char windowtitle_[255] = "hello, circle";
+  std::queue<SDL_Showable> draw_queue_;
+  bool show_settings_ = false;
 
 public:
   App() {
@@ -94,56 +141,6 @@ public:
   App &operator=(const App &) = delete;
   App &operator=(App &&) = delete;
 
-  void frame() {
-    SDL_SetRenderDrawColor(ren_, 0x00, 0x00, 0x00, 0xFF);
-    SDL_RenderClear(ren_);
-
-    SDL_SetRenderDrawColor(ren_, 0xFF, 0xFF, 0xFF, 0xFF);
-    Showable<SDL_Renderer *> sh(
-        ColoredCircle{{{150, 150}, 150}, 0xFF, 0x00, 0xFF, 0xFF});
-    std::vector<Showable<SDL_Renderer *>> shl = {Circle{{150, 150}, 150}};
-    shl.push_back(ColoredCircle{{{150, 250}, 150}, 0xFF, 0x00, 0xFF, 0xFF});
-
-    for (auto &shi : shl)
-      Show(ren_, shi);
-
-    ImGui_ImplSDLRenderer2_RenderDrawData(
-      ImGui::GetDrawData()
-    );
-
-    SDL_RenderPresent(ren_);
-    // SDL_Delay(2000);
-  }
-
-  void run() {
-    running_ = true;
-    bool show_panel{true};
-    while (running_) {
-    SDL_Event e{};
-      while (SDL_PollEvent(&e) != 0) {
-        ImGui_ImplSDL2_ProcessEvent(&e);
-        if (e.type == SDL_QUIT)
-          stop();
-      }
-
-      ImGui_ImplSDLRenderer2_NewFrame();
-      ImGui_ImplSDL2_NewFrame();
-      ImGui::NewFrame();
-
-      if (show_panel) {
-        ImGui::Begin("Some panel", &show_panel);
-        ImGui::Text("Hello, world");
-        ImGui::End();
-      }
-      ImGui::Render();
-      frame();
-    }
-  }
-
-  void stop() {
-    running_ = false;
-  }
-
   ~App() {
     ImGui_ImplSDLRenderer2_Shutdown();
     ImGui_ImplSDL2_Shutdown();
@@ -152,6 +149,68 @@ public:
     SDL_DestroyRenderer(ren_);
     SDL_DestroyWindow(win_);
   }
+
+  void run() {
+    running_ = true;
+    while (running_) {
+      processEvent();
+      imGuiRender();
+      update();
+      render();
+    }
+  }
+
+private:
+  void processEvent() {
+    SDL_Event e{};
+    while (SDL_PollEvent(&e) != 0) {
+      ImGui_ImplSDL2_ProcessEvent(&e);
+      if (e.type == SDL_QUIT)
+        running_ = false;
+    }
+  }
+
+  void update() {}
+
+  void imGuiRender() {
+    ImGui_ImplSDLRenderer2_NewFrame();
+    ImGui_ImplSDL2_NewFrame();
+    ImGui::NewFrame();
+
+    if (ImGui::BeginMainMenuBar()) {
+      if (ImGui::BeginMenu("Window")) {
+        ImGui::MenuItem("Settings", nullptr, &show_settings_);
+        ImGui::EndMenu();
+      }
+      ImGui::EndMainMenuBar();
+    }
+    if (show_settings_) {
+      ImGui::Begin("Some panel", &show_settings_);
+      ImGui::Text("Hello, world");
+      ImGui::ColorEdit3("Backgound color", color_);
+      ImGui::InputText("Window title", windowtitle_, 255);
+      if (ImGui::Button("Update window title"))
+        SDL_SetWindowTitle(win_, windowtitle_);
+      ImGui::End();
+    }
+    ImGui::Render();
+  }
+
+  void render() {
+    SDL_SetRenderDrawColor(ren_, static_cast<uint8_t>(color_[0] * 255),
+                           static_cast<uint8_t>(color_[1] * 255),
+                           static_cast<uint8_t>(color_[2] * 255), 0xFF);
+    SDL_RenderClear(ren_);
+
+    while (!draw_queue_.empty()) {
+      Show(ren_, draw_queue_.front());
+      draw_queue_.pop();
+    }
+
+    ImGui_ImplSDLRenderer2_RenderDrawData(ImGui::GetDrawData());
+
+    SDL_RenderPresent(ren_);
+  }
 };
 
 int main(int argc, char **argv) try {
@@ -159,7 +218,6 @@ int main(int argc, char **argv) try {
     throw sdl_error("SDL_Init");
   {
     App app;
-    // app.frame();
     app.run();
   }
   SDL_Quit();
